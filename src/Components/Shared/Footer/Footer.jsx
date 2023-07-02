b2259cd552cc54d8659264eb63c8de0a3d4248bc
YusufHasan94 <yhpolok@gmail.com>
2023-07-02 12:16:58 +0600

    update foorer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -2,9 +2,8 @@ import { FaRegCopyright } from "react-icons/fa";
 const Footer = () => {
     return (
         <footer className="footer p-10 flex items-center justify-center">
-            <p className="flex items-center text-xl">
-                <FaRegCopyright></FaRegCopyright>
-                2023. All rights reserved by Yusuf Hasan
+            <p className="flex items-center text-base md:text-xl">
+                <FaRegCopyright></FaRegCopyright> 2023. All rights reserved by Yusuf Hasan
             </p>
             
         </footer>
